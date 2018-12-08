4cf163d4479addaf6337c2fb0baeab77e00e53f6
Praveen Oak <praveenoak@10-16-239-173.dynapool.wireless.nyu.edu>
2018-12-08 10:04:27 -0500

    WIP

---
+++ clean_data.py
@@ -0,0 +1,109 @@
+
+import time
+from datetime import datetime
+
+date_pattern = '%Y-%m-%d %H:%M:%S'
+#date format : 2012-05-12 05:09:05.0000002
+def convert_date_to_epoch(date_string):
+	if len(date_string) > 19:
+		raise Exception("Date String not in right format") 
+
+	epoch = int(time.mktime(time.strptime(date_string, date_pattern)))
+	return epoch
+
+def get_time_of_day_from_epoch(epoch):
+	return epoch % (24 * 60 * 60)
+
+def get_is_date_string_weekend(date_string):
+	datetime_object = datetime.strptime(date_string, date_pattern)
+	if datetime_object.weekday() < 5:
+		return 0
+	else:
+		return 1
+
+
+def get_number_of_lines(filename):
+	file_pointer = open(filename, "r")
+	index = 0
+	while True:
+		line = file_pointer.readline()
+		if line == "":
+			break
+		index = index + 1
+		if index % 10000000 == 0:
+			print line
+
+	print index
+
+def clean_date_string(date_string):
+	return date_string[:19]
+
+
+def process_entry(file_string):
+	string_array = file_string.split(",")
+
+	start_date_string = clean_date_string(string_array[2])
+	start_time_epoch = convert_date_to_epoch(start_date_string)
+	start_time_of_day = get_time_of_day_from_epoch(start_time_epoch)
+	start_time_is_weekend = get_is_date_string_weekend(start_date_string)
+
+	fare = float(string_array[1])
+	pickup_longitude = float(string_array[3])
+	pickup_latitude = float(string_array[4])
+
+	dropoff_longitude = float(string_array[5])
+	dropoff_latitude = float(string_array[6])
+
+	passengers = string_array[7]
+
+	new_array = [fare, start_time_of_day, start_time_is_weekend, pickup_longitude, pickup_latitude, dropoff_longitude, dropoff_latitude ]
+	return new_array
+
+def process_for_dates(source_file, destination_file):
+	source_pointer = open(source_file, "r")
+	dest_pointer = open(destination_file, "w")
+
+	index = 0
+	while True:
+		try:
+			source_line = source_pointer.readline()
+
+			if source_line == "" or index > 100:
+				source_pointer.close()
+				dest_pointer.close()
+				break
+
+			dest_format_array = process_entry(source_line)
+			dest_format_array.insert(0,index)
+			#create a string out of array
+			dest_format_string = ','.join(str(node) for node in dest_format_array) + '\n'
+			dest_pointer.write(dest_format_string)
+
+			index = index + 1
+		except ValueError as e:
+			print e
+			print "Error parsing file with index = "+str(index)
+
+	
+
+def main():
+	data = open("train.csv", "r+")
+
+	new_file = []
+	index = 0
+	while index < 10000:
+		new_file.append(data.readline(index))
+		index = index + 1
+
+	file_object  = open("smaller.csv", "w+")
+
+	for line in new_file:
+		file_object.write(line)
+
+if __name__== "__main__":
+  process_for_dates("train.csv", "train_dates_done.csv")
+
+
+
+
+
